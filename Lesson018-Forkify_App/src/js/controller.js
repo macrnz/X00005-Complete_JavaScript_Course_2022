184f7d72758d260c0616dc5d9d6a73ba7ddf91ed
macrnz <markrenzcarillo@gmail.com>
2022-06-27 16:23:08 +0800

    progress

--- Lesson018-Forkify_App/src/js/controller.js
+++ Lesson018-Forkify_App/src/js/controller.js
@@ -1,13 +1,46 @@
-const recipeContainer = document.querySelector('.recipe');
+import * as model from "./model.js";
+import recipeView from "./views/recipeView.js";
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
+//import icons from "../img/icons.svg"; //PARCEL 1
+import icons from "url:../img/icons.svg"; //PARCEL 2
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+//console.log(icons);
+
+const recipeContainer = document.querySelector(".recipe");
 
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+//console.log("test message");
+
+const controlRecipes = async function () {
+  try {
+    //ID GENERATOR
+    const id = window.location.hash.slice(1);
+    if (!id) return;
+
+    //LOAD SPINNER
+    recipeView.renderSpinner();
+
+    //LOADING RECIPE
+    await model.loadRecipe(id);
+    const { recipe } = model.state;
+
+    //RENDERING RECIPE
+    recipeView.render(model.state.recipe);
+    //const recipeView = new RecipeView(model)
+  } catch (err) {
+    alert(err);
+  }
+};
+//controlRecipes();
+
+//ACTIVE CODES
+//SIMPLIFIED
+["hashchange", "load"].forEach((ev) =>
+  window.addEventListener(ev, controlRecipes)
+);
+//ORIGINAL CODE BELOW
+//window.addEventListener("hashchange", controlRecipes);
+//window.addEventListener("load", controlRecipes);
---
+++ Lesson018-Forkify_App/src/js/views/helpers.js
@@ -0,0 +1,26 @@
+import { async } from "regenerator-runtime";
+import { TIMEOUT_SEC } from "../config";
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
+export const getJSON = async function (url) {
+  try {
+    //LOADING RECIPE
+    const res = await Promise.race([fetch(url), timeout(TIMEOUT_SEC)]);
+    //fetch(url);
+    //"https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886"
+    //"https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcd86"
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    return data;
+  } catch (err) {
+    throw err;
+  }
+};
